19acd0937b626508de0c5ab253ef4afbeb6b7a41
jmaldanat <jmaldanat@unal.edu.co>
2025-06-30 10:58:34 -0500

    Add Procfile and update settings for production deployment on Heroku

--- codestar/settings.py
+++ codestar/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8j8%_#gm@iiwp3hjm1915#pi*-u5y@6$k^^qdpf$j4)!#bbnp9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com','127.0.0.1',]
 
 
 # Application definition
