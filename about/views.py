38100092908f991e3351122464836f03ed33d600
jmaldanat <jmaldanat@unal.edu.co>
2025-07-06 08:39:01 -0500

    Add CollaborateRequest model, form, and admin integration; update About view and template for collaboration requests

--- about/views.py
+++ about/views.py
@@ -1,14 +1,31 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
 from .models import About
+from .forms import CollaborateForm
 
 def about_me(request):
     """
     Renders the About page
     """
+
+    if request.method == "POST":
+        print("Received a POST request")
+        collaborate_form = CollaborateForm(data=request.POST)
+        if collaborate_form.is_valid():
+            collaborate_form.save()
+            messages.add_message(
+            request, messages.SUCCESS,
+            'Collaboration request received! I endeavor to respond within 2 working days.'
+            )
+
     about = About.objects.all().order_by('-updated_on').first()
+    collaborate_form = CollaborateForm()
 
     return render(
         request,
         "about/about.html",
-        {"about": about},
+        {
+            "about": about,
+            "collaborate_form": collaborate_form
+         },
     )
